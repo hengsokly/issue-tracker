f390f5bebcace129db99dfebc9af99439d702552
hengsokly <hengsokly23@gmail.com>
2024-03-01 16:05:40 +0700

    Implement client side validation

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,15 +1,21 @@
 'use client'
-import { Button, TextArea, TextField } from '@radix-ui/themes'
+import { Button, Callout, TextArea, TextField, Text } from '@radix-ui/themes'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchemas';
+import { z } from "zod";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
+
+// interface IssueForm {
+//   title: string;
+//   description: string;
+// }
 
 const NewIssuePage = () => {
   const {
@@ -23,30 +29,50 @@ const NewIssuePage = () => {
       title: "",
       description: ""
     },
+    resolver: zodResolver(createIssueSchema)
   })
+
+  const [error, setError] = useState('');
 
   const router = useRouter();
 
   const onSubmit: SubmitHandler<IssueForm> = async (data) => {
-    const response = await axios.post('/api/issues', data);
-    router.push('/issues');
+    try {
+      const response = await axios.post('/api/issues', data);
+      router.push('/issues');
+    } catch (error) {
+      console.log(error);
+      setError('An unexpect error occure!')
+    }
   }
 
   return (
-    <form className='max-w-xl space-y-3'
-      onSubmit={handleSubmit(onSubmit)}>
-      <TextField.Root>
-        <TextField.Input placeholder="Search the docs…" {...register('title')} />
-      </TextField.Root>
+    <div className='max-w-xl'>
+      {error && 
+        <Callout.Root color='red' className='mb-5'>
+          <Callout.Text>
+            You will need admin privileges to install and access this application.
+          </Callout.Text>
+        </Callout.Root>
+      }
 
-      <Controller 
-        name="description"
-        control={control}
-        render={({field}) => <SimpleMDE placeholder='Description' {...field} />}
-      />
+      <form className=' space-y-3'
+        onSubmit={handleSubmit(onSubmit)}>
+        <TextField.Root>
+          <TextField.Input placeholder="Search the docs…" {...register('title')} />
+        </TextField.Root>
+        {errors.title && <Text color='red'>{errors.title.message}</Text>}
 
-      <Button>Submit new issue</Button>
-    </form>
+        <Controller 
+          name="description"
+          control={control}
+          render={({field}) => <SimpleMDE placeholder='Description' {...field} />}
+        />
+        {errors.description && <Text color='red' as='p'>{errors.description.message}</Text>}
+
+        <Button>Submit new issue</Button>
+      </form>
+    </div>
   )
 }
 
