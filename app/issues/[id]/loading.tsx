6977b841df64b8b2ec7a0208fdeba1fc5f491559
hengsokly <hengsokly23@gmail.com>
2024-03-04 09:48:32 +0700

    Show issue detail

---
+++ app/issues/[id]/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div>
+      Loading issue detail...
+    </div>
+  )
+}
+
+export default loading
---
+++ app/issues/new/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoadingIssueNew = () => {
+  return (
+    <div>
+      Loading New Issue
+    </div>
+  )
+}
+
+export default LoadingIssueNew
