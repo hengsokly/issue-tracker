00d7e85a94a855c91f990d4d3605157c3ee86f27
hengsokly <hengsokly23@gmail.com>
2024-03-11 09:31:45 +0700

    Add assigned issue to prisma

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -7,23 +7,23 @@ import axios from 'axios';
 import { useQuery } from 'react-query';
 
 const AssigneeSelect = () => {
-  const {data: users, error, isLoading} = useQuery<User[]>({
+  const { data: users, error, isLoading } = useQuery<User[]>({
     queryKey: ['users'],
     queryFn: () => axios.get<User[]>('/api/users').then(res => res.data),
     staleTime: 60 * 1000, //60 seconds
     retry: 3
   })
 
-  if(isLoading) return <Skeleton />
+  if (isLoading) return <Skeleton />
 
-  if(error) return null;
+  if (error) return null;
 
   // useEffect(() => {
   //   const fetchUser = async () => {
   //     const {data} = await axios.get<User[]>('/api/users');
   //     setUsers(data);
   //   }
-    
+
   //   fetchUser();
   // }, [])
 
@@ -33,7 +33,7 @@ const AssigneeSelect = () => {
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          { users?.map(user => <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>) }
+          {users?.map(user => <Select.Item key={user.id} value={user.id}>{user.name}</Select.Item>)}
         </Select.Group>
       </Select.Content>
     </Select.Root>
