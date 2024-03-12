3f947b09f9eb1f270f2bbb041ab0c4377195c656
hengsokly <hengsokly23@gmail.com>
2024-03-12 16:13:29 +0700

    Extract issue table into a component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,26 +1,14 @@
-import { IssueStatusBadge, Link } from "@/app/components";
 import prisma from "@/prisma/client";
 import { Status } from "@prisma/client";
-import { ArrowUpIcon } from "@radix-ui/react-icons";
-import { Table } from "@radix-ui/themes";
-import NextLink from "next/link";
 import IssueActions from "./IssueActions";
+import IssueTable, { IssueQuery, columnNames } from "./IssueTable";
 import Pagination from "./_components/Pagination";
+import { Flex } from "@radix-ui/themes";
 
 const statuses = Object.values(Status);
-const columns: { label: string; value: keyof Issue; className?: string }[] = [
-  { label: "Issue", value: "title" },
-  { label: "Status", value: "status", className: "hidden md:table-cell" },
-  { label: "Created At", value: "createdAt", className: "hidden md:table-cell" },
-];
-
 interface Props {
   // All search Params are string type, that's why page is pass with string
-  searchParams: { 
-    status: Status; 
-    orderBy: keyof Issue;
-    page: string;
-  }
+  searchParams: IssueQuery
 }
 
 const IssuesPage = async ({searchParams}: Props) => {
@@ -30,7 +18,7 @@ const IssuesPage = async ({searchParams}: Props) => {
 
   const page = parseInt(searchParams.page) || 1;
   const pageSize = 10;
-  const orderBy = columns.map(column => column.value).includes(searchParams.orderBy) ? {[searchParams.orderBy]: 'asc'} : undefined;
+  const orderBy = columnNames.map(column => column).includes(searchParams.orderBy) ? {[searchParams.orderBy]: 'asc'} : undefined;
   const issues = await prisma.issue.findMany({
     where: { status: status },
     orderBy: orderBy,
@@ -45,55 +33,11 @@ const IssuesPage = async ({searchParams}: Props) => {
   // await delay(2000);
 
   return (
-    <div>
+    <Flex direction={'column'} gap={'3'}>
       <IssueActions></IssueActions>
-
-      <Table.Root variant="surface">
-        <Table.Header>
-          <Table.Row>
-            {columns.map((column) => (
-              <Table.ColumnHeaderCell
-                key={column.value}
-                className={column.className}
-              >
-                <NextLink
-                  href={{
-                    query: { ...searchParams, orderBy: column.value },
-                  }}
-                >
-                  {column.label}
-                  {searchParams.orderBy == column.value && (
-                    <ArrowUpIcon className="inline" />
-                  )}
-                </NextLink>
-              </Table.ColumnHeaderCell>
-            ))}
-          </Table.Row>
-        </Table.Header>
-
-        <Table.Body>
-          {issues.map((issue) => (
-            <Table.Row key={issue.id}>
-              <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
-                <div className="block md:hidden">
-                  <IssueStatusBadge status={issue.status}></IssueStatusBadge>
-                </div>
-              </Table.Cell>
-
-              <Table.Cell className="hidden md:table-cell">
-                <IssueStatusBadge status={issue.status}></IssueStatusBadge>
-              </Table.Cell>
-              <Table.Cell className="hidden md:table-cell">
-                {issue.createdAt.toDateString()}
-              </Table.Cell>
-            </Table.Row>
-          ))}
-        </Table.Body>
-      </Table.Root>
-
+      <IssueTable searchParams={searchParams} issues={issues}></IssueTable>
       <Pagination currentPage={page} itemCount={issueCount} pageSize={pageSize}/>
-    </div>
+    </Flex>
   );
 };
 // Tell next to render the route as dynamic
