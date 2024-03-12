3f205859519e318ff1c8cfeb0f9896d87505b4e1
hengsokly <hengsokly23@gmail.com>
2024-03-12 11:37:10 +0700

    Implement pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import Pagination from "./issues/_components/Pagination";
+import { Button } from "@radix-ui/themes";
+import { DoubleArrowLeftIcon } from "@radix-ui/react-icons";
 
-export default function Home() {
+export default function Home({searchParams}: {searchParams: {page: string}}) {
   return (
     <div>
       Hello Home page
+
+      <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)}/>
+      
     </div>
   );
 }
