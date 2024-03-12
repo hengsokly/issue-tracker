8fa65edb05b8390821a2cd5a5a772233cbdb8d92
hengsokly <hengsokly23@gmail.com>
2024-03-12 09:44:50 +0700

    Filter issue

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,11 +1,23 @@
-import prisma from '@/prisma/client';
-import { Table } from '@radix-ui/themes';
-import {IssueStatusBadge, Link} from '@/app/components';
-import IssueActions from './IssueActions';
+import prisma from "@/prisma/client";
+import { Table } from "@radix-ui/themes";
+import { IssueStatusBadge, Link } from "@/app/components";
+import IssueActions from "./IssueActions";
+import { Status } from "@prisma/client";
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
-  
+const statuses = Object.values(Status);
+
+const IssuesPage = async ({
+  searchParams,
+}: {
+  searchParams: { status: Status };
+}) => {
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+  const issues = await prisma.issue.findMany({
+    where: { status },
+  });
+
   // this is for delay time to see loading page
   // Should remove it when shipping your code to production
   // await delay(2000);
@@ -14,39 +26,43 @@ const IssuesPage = async () => {
     <div>
       <IssueActions></IssueActions>
 
-      <Table.Root variant='surface'>
+      <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
             <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
           </Table.Row>
         </Table.Header>
-        
+
         <Table.Body>
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                <Link href={`/issues/${issue.id}`}>
-                  {issue.title}
-                </Link>
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                 <div className="block md:hidden">
-                  <IssueStatusBadge status={issue.status}></IssueStatusBadge>  
+                  <IssueStatusBadge status={issue.status}></IssueStatusBadge>
                 </div>
               </Table.Cell>
 
-              <Table.Cell className='hidden md:table-cell'>
-                <IssueStatusBadge status={issue.status}></IssueStatusBadge>  
+              <Table.Cell className="hidden md:table-cell">
+                <IssueStatusBadge status={issue.status}></IssueStatusBadge>
               </Table.Cell>
-              <Table.Cell className='hidden md:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
             </Table.Row>
           ))}
         </Table.Body>
       </Table.Root>
     </div>
-  )
-}
+  );
+};
 // Tell next to render the route as dynamic
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
-export default IssuesPage
+export default IssuesPage;
